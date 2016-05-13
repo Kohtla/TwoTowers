47bf3725fbdb3c3da675e40a735848a7deb3bbfe
Kohtla <MKmoolooo@yandex.ru>
2016-05-13 20:00:11 +0400

    First_bitch
    Самое первое горячее дополнение. Ничего не работает. Что то там слегка
    задал для класса space. Каждый скрипт в Unity - автоматом создает класс
    и наследует его от класса Monobehavior,или как-то так

---
+++ Assets/Classes/Space.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Space {
+	bool isWall = false;
+	bool isUnit = false;
+	bool isBuilding = false;
+	bool isInvisible = false;
+	public void setUnit()
+	{
+	}
+	public void killUnit()
+	{
+	}
+	public void setBuild()
+	{
+	}
+	public void killBuild()
+	{		
+	}
+	public bool isEmpty()
+	{		
+	}
+
+}
+
+
